4d4d4ee0def55d38c9b17e741be0f6e88083aa3e
Erin Zimmer <erin.zimmer@auspost.com.au>
2017-08-09 18:49:24 +1000

    branches

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -1,40 +1,53 @@
 import Terminal from '../terminal.js';
+//import sha1 from '../node-modules/sha1/sha1.js';
+
+const seed = 0;
 
 export default class Checkout {
 
   constructor() {
-    this.keyEvents = {
-      'git checkout -b': this.checkout.bind(this),
+
+    this.files = {
+      '.git/HEAD': 'refs/heads/master',
+      '.git/refs/heads/master': '12345',
     };
 
-    document.getElementById('content').addEventListener('click', this.showRefs, { once: true });
-  }
-
-  showRefs(event) {
-    event.stopPropagation();
-    const refs = document.getElementById('refs');
-    refs.classList.add('fade-in');
-
-    document.getElementById('content').addEventListener('click', this.showHead, { once: true });
-  }
-
-  showHead(event) {
-    event.stopPropagation();
-    document.getElementById('head').classList.add('fade-in');
-
+    this.keyEvents = {
+      'git checkout -b': this.checkout.bind(this),
+      'git commit': this.commit.bind(this),
+      'cat': this.cat.bind(this),
+    };
     this.terminal = new Terminal(this.keyEvents);
   }
 
+  get currentBranch() {
+    return this.files['.git/HEAD'];
+  }
+
   checkout(branchName) {
+    const currentHash = this.files[`.git/${this.currentBranch}`];
+
+    this.files[`.git/refs/heads/${branchName}`] = currentHash;
+    this.files['.git/HEAD'] = branchName;
+
     this.terminal.echo(`Switched to a new branch '${branchName}'`);
-    document.getElementById('image').classList.add('slide-up-animation');
+  }
+
+  cat(fileName) {
+    this.terminal.echo(this.files[fileName]);
+  }
+
+  commit() {
+    const nextCommit = sha1(seed++);
+    this.files[`.git/`]
   }
 
   static getTemplate() {
-    return `<div class="images">
-              <img id="refs" class="hiding" src="images/new-branch.png" />
-              <img id="head" class="hiding" src="images/new-branch-head.png" />
-            </div>
-            <div class="terminal"></div>`;
+    return `<div class="columns">
+              <div class="terminal"></div>
+              <div class="overflow">
+                <img id="refs" src="images/new-branch-head.png" />
+              </div>
+            </div>`;
   }
 }
--- controllers/hashes.js
+++ controllers/hashes.js
@@ -6,7 +6,7 @@ export default class HashesController {
     this.keyEvents = {
       'git add .': this.gitAdd.bind(this),
       'git hash-object': this.gitHashObject.bind(this),
-      'git cat-file': this.gitCatFile.bind(this),
+      'git cat-file -p': this.gitCatFile.bind(this),
       'cat': this.catFile.bind(this),
       'git ls-files': this.gitLsFiles.bind(this),
       'git status': this.gitStatus.bind(this),
@@ -21,7 +21,6 @@ export default class HashesController {
   }
 
   gitHashObject() {
-    this.show('index-html-in-repo');
     this.terminal.echo('c50eddd41faba2ecc8928e459288fe612b999170');
   }
 
@@ -34,9 +33,18 @@ H�2�в=gT��>V�%`);
     }
   }
 
-  gitCatFile() {
-    this.show('cat-file');
-    this.terminal.showPrompt();
+  gitCatFile(hash) {
+    if (!this.cats) {
+      this.cats = true;
+      this.show('cat-file');
+      return;
+    }
+
+    this.show('app-css-in-repo');
+    this.terminal.echo(`
+body {
+  background-color: papayawhip;
+}`);
   }
 
   gitLsFiles() {
@@ -47,11 +55,6 @@ index.html`);
   }
 
   gitStatus() {
-    document.querySelectorAll('.images img')
-      .forEach(img => {
-        img.classList = 'disappear';
-      });    
-
     this.terminal.echo(`
 On branch master
 Changes to be committed:
@@ -77,7 +80,7 @@ Changes to be committed:
         <div class="images">
           <img class="slide-down" src="images/clean-git-repo.png" />
           <img id="after-add-repo" class="hiding" src="images/after-add-repo.png" />
-          <img id="index-html-in-repo" class="hiding" src="images/index-html-in-repo.png" />
+          <img id="app-css-in-repo" class="hiding" src="images/app-css-in-repo.png" />
           <img id="cat-file" class="hiding" src="images/cat-file.jpeg" />
         </div>
         <div class="terminal"></div>
